67a1e76f843752b70feba227958f0edf55bba033
OmarMansour557 <omarmansour557@gmail.com>
2021-12-05 04:16:41 +0200

    changing the function getError to make the overlapping between chunks

--- Error.py
+++ Error.py
@@ -2,27 +2,52 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-length_of_data=401
+length_of_data=1000
 loaded_ECG_data = pd.read_csv("emg.csv")
 filtered_ECG_mV = loaded_ECG_data['filtered_ECG_mV']
 time = loaded_ECG_data['time_sec']
-def getError(function_degree,no_of_chuncks):
+def getError(function_degree,no_of_chuncks,overlapping):
     total_Residual = 0
-    
-
     intervals = int(length_of_data/no_of_chuncks)
     for i in range(no_of_chuncks):
-        coefficients, residual, _, _, _  = np.polyfit(time[i*intervals:intervals*(i+1)-1],filtered_ECG_mV[0+i*intervals:intervals*(i+1)-1],function_degree,full='true')
-        print(residual)
-        total_Residual = total_Residual + residual
-    return total_Residual/length_of_data
+        if i == 0:
+            coefficients, residual, _, _, _  = np.polyfit(time[i*intervals:intervals*(i+1)-1],filtered_ECG_mV[0+i*intervals:intervals*(i+1)-1],function_degree,full='true')
+            total_Residual = total_Residual + residual
+            # for illustration
+            # print(i*intervals)
+            # print(intervals*(i+1)-1)
+
+
+        elif i<no_of_chuncks-1:
+            coefficients, residual, _, _, _  = np.polyfit(time[i*intervals-int(i*intervals*(overlapping/100)):intervals*(i+1)-1-int(i*intervals*(overlapping/100))]
+            ,filtered_ECG_mV[i*intervals-int(i*intervals*(overlapping/100)):intervals*(i+1)-1-int(i*intervals*(overlapping/100))],function_degree,full='true')
+            total_Residual = total_Residual + residual
+            # for illustration
+            # print(i*intervals-int(i*intervals*(overlapping/100)))
+            # print(intervals*(i+1)-1-int(i*intervals*(overlapping/100)))
+
+        else:
+            coefficients, residual, _, _, _  = np.polyfit(time[intervals*(i)-1-int((i-1)*intervals*(overlapping/100)):length_of_data]
+            ,filtered_ECG_mV[intervals*(i)-1-int((i-1)*intervals*(overlapping/100)):length_of_data],function_degree,full='true')
+            total_Residual = total_Residual + residual
+
+            # for illustration
+            # x = intervals*(i)-1-int((i-1)*intervals*(overlapping/100))
+            # x = 1000-x
+            # print(x)
+
+
+
+
+    return total_Residual/np.sqrt(length_of_data)
         
     
-print(getError(4,4))
 
 
 
 
+# print(getError(4,6,90))
+# print(getError(4,6,0))
 
 
 
